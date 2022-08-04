faed2164d09c9699ea3e6c44cf7df3c6b342f6bf
Ben Mensah <benlottejnr@gmail.com>
2022-08-04 14:42:09 +0000

    finalizing module that deploys aws vpc

--- setups/main.tf
+++ setups/main.tf
@@ -15,12 +15,6 @@ provider "aws" {
 module "bens_webserver" {
   source             = "../modules/webserver"
 
-  vpc_cidr_block     = "10.0.0.0/16"
-  subneta_cidr_block = "10.0.1.0/24"
-  subnetb_cidr_block = "10.0.2.0/24"
-  availability_zone  = "us-east-1"
   devclass_name      = "ben10"
-  ami                = "ami-052efd3df9dad4825"
-  instance_type      = "t2.micro"
 }
 
